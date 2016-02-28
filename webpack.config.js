f29eef026efc186fe9b5f61fe6b6455090c7c3fe
Aydrian J. Howard <aydrian@gmail.com>
2016-02-28 15:54:14 -0500

    moved babel config to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ }
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   }
 }
